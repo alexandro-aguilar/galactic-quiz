460b05655359d05cf31b0433ee7a949f29b46622
Alexandro Aguilar <alexandro.aguilar@caylent.com>
2025-03-17 14:59:58 -0600

    chore: update

---
+++ app/modules/quiz/get/GetParaphrasedQuizRepository.ts
@@ -0,0 +1,37 @@
+import Quiz from './Quiz';
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import QuizQuestionsDTO from './QuizQuestionsDTO';
+import ParaphraseRepository from './ParaphraseRepository';
+import Environment from '../../../utils/Environment';
+
+export default class GetQuizRepository {
+  
+  private readonly client: S3Client;
+  private readonly bucket: string;
+  private readonly paraphraseRepository: ParaphraseRepository;
+
+  constructor() {
+    this.client = new S3Client({ region: 'us-east-1' });
+    this.bucket = Environment.QuizBucket;
+    this.paraphraseRepository = new ParaphraseRepository();
+  }
+
+  async execute(type: number): Promise<Quiz> {
+    const command = new GetObjectCommand({
+      Bucket: this.bucket,
+      Key: `${type}.json`
+    });
+
+    try {
+      const response = await this.client.send(command);
+      const file = await response.Body?.transformToString() as string;
+      const questions: Array<QuizQuestionsDTO> = JSON.parse(file);
+      const quiz = Quiz.fromObject(questions);
+      const paraphrasedQuiz = await this.paraphraseRepository.execute(quiz.questions);
+      return paraphrasedQuiz;
+    } catch (error) {
+      console.error('Error getting object:', error);
+      throw error;
+    }
+  }
+}
