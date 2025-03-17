628d22e0f27430b93c003fdd6bbbe7e0f552612e
Alexandro Aguilar <alexandro.aguilar@caylent.com>
2025-03-17 09:36:00 -0600

    feat: test config add

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,25 @@
 module.exports = {
+  preset: 'ts-jest', // If you want to use TypeScript for the Jest configuration itself
   testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testRegex: '\\.(test|spec)\\.[t]s$', // Regex for test files
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+  collectCoverageFrom: ['app/directory/**/*.{ts,tsx}', '!**/node_modules/**', '!**/*.d.ts', '!**/I*'],
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/app/$1', // Map @app to the project root
+    '^@utils/(.*)$': '<rootDir>/app/utils/$1', // Map @app to the project root
+  },
 };
+
+// const path = require('path');
+
+// module.exports = {
+//   rootDir: path.resolve(__dirname),
+// };
