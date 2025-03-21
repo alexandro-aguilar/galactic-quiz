d2a00a88ac949d08dab4525307ec20951ee59b10
Alexandro Aguilar <alexandro.aguilar@caylent.com>
2025-03-21 12:07:26 -0600

    test: add tests for GetQuizController

--- test/app/modules/quiz/get/getQuizController/BaseCase.spec.ts
+++ test/app/modules/quiz/get/getQuizController/BaseCase.spec.ts
@@ -1,46 +1,9 @@
 import GetQuizController from '@app/modules/quiz/get/GetQuizController';
 import { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2 } from 'aws-lambda';
+import { fixtures, fixtures2, mockQuiz } from './fixtures';
 
-describe('GetQuizController - Happy Path', () => {
+describe('GetQuizController - Base Case', () => {
   let getQuizController: GetQuizController;
-
-  const mockQuiz = {
-    toJson: () => ([
-      {
-        id: '1',
-        type: 'multiple-choice',
-        text: 'What is 2 + 2?',
-        answers: [
-          { id: 'a', text: '3' },
-          { id: 'b', text: '4' },
-          { id: 'c', text: '5' },
-        ],
-        rightAnswer: 'b',
-      },
-      {
-        id: '2',
-        type: 'multiple-choice',
-        text: 'What is 3 + 3?',
-        answers: [
-          { id: 'a', text: '5' },
-          { id: 'b', text: '6' },
-          { id: 'c', text: '7' },
-        ],
-        rightAnswer: 'b',
-      },
-      {
-        id: '3',
-        type: 'multiple-choice',
-        text: 'What is 4 + 4?',
-        answers: [
-          { id: 'a', text: '7' },
-          { id: 'b', text: '8' },
-          { id: 'c', text: '9' },
-        ],
-        rightAnswer: 'b',
-      },
-    ]),
-  };
 
   const mockGetQuizRepository = {
     execute: jest.fn().mockResolvedValue(mockQuiz),
@@ -51,18 +14,43 @@ describe('GetQuizController - Happy Path', () => {
     getQuizController = new GetQuizController(mockGetQuizRepository as any);
   });
 
-  test.each([
-    { pathParameters: { type: 1 }, headers: {}, expectedStatus: 200, expectedResponse: JSON.stringify(mockQuiz.toJson()) },
-    { pathParameters: { type: 2 }, headers: {}, expectedStatus: 200, expectedResponse: JSON.stringify(mockQuiz.toJson()) },
-    { pathParameters: { type: 3 }, headers: {}, expectedStatus: 200, expectedResponse: JSON.stringify(mockQuiz.toJson()) },
-  ])(
-    'Given a valid quizId ($quizId), When the controller is called, Then it should return status $expectedStatus and the correct quiz data',
+  test.each(fixtures)(
+    'Given a type, When the controller is called, Then it should return status $expectedStatus and the correct quiz data',
     async ({ pathParameters, headers, expectedStatus, expectedResponse }) => {
       // Arrange
       const request = { pathParameters, headers } as unknown as APIGatewayProxyEventV2;
 
       // Act
       const response: APIGatewayProxyStructuredResultV2 = await getQuizController.execute(request) as APIGatewayProxyStructuredResultV2;
+      
+      // Assert
+      expect(response.statusCode).toBe(expectedStatus);
+      expect(response.body).toBe(expectedResponse);
+    }
+  );
+});
+
+describe('GetQuizController - Base Case', () => {
+  let getQuizController: GetQuizController;
+
+  const mockGetQuizRepository = {
+    execute: () => { throw new Error('Error getting object'); },
+  };
+
+  beforeEach(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    getQuizController = new GetQuizController(mockGetQuizRepository as any);
+  });
+
+  test.each(fixtures2)(
+    'Given a controller call, When the controller invokes the Repository and it throws an error, Then it should return status $expectedStatus and the correct error data',
+    async ({ pathParameters, headers, expectedStatus, expectedResponse }) => {
+      // Arrange
+      const request = { pathParameters, headers } as unknown as APIGatewayProxyEventV2;
+
+      // Act
+      const response: APIGatewayProxyStructuredResultV2 = await getQuizController.execute(request) as APIGatewayProxyStructuredResultV2;
+      console.log(response);
       
       // Assert
       expect(response.statusCode).toBe(expectedStatus);
