9e6517bed54963f33c6921b1e55f465c9a1f741e
Alexandro Aguilar <alexandro.aguilar@caylent.com>
2025-03-26 13:05:44 -0600

    refactor: name convention

--- app/modules/user/register/UserRegisterHandler.ts
+++
@@ -1,8 +0,0 @@
-import UserRegisterController from './UserRegisterController';
-import { APIGatewayProxyEventV2 } from 'aws-lambda';
-
-export const handler = async (event: APIGatewayProxyEventV2) => {
-  const userRegisterController = new UserRegisterController();
-  const response = await userRegisterController.execute(event);
-  return response;
-};
