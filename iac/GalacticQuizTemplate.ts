9218cf7b74d746cd05d5fcc440d6275d553f43d0
Alexandro Aguilar <alexandro.aguilar@caylent.com>
2025-03-26 11:24:51 -0600

    chore: update

--- iac/GalacticQuizTemplate.ts
+++ iac/GalacticQuizTemplate.ts
@@ -3,11 +3,13 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import GalacticQuizStack from './GalacticQuizStack';
 import * as dotenv from 'dotenv';
+import Environment from './utils/Environment';
 
 dotenv.config();
 // Todo: borrar stack con el viejo nombre en todas las cuentas AWS
 const app = new cdk.App();
-new GalacticQuizStack(app, 'GalacticQuiz', {
+console.log('Environment', Environment);
+new GalacticQuizStack(app, Environment.projectName, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
