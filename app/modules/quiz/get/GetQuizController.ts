9863cd6e05a03f2026d3bb31fae1abb9347cb95b
Alexandro Aguilar <alexandro.aguilar@caylent.com>
2025-03-26 13:32:30 -0600

    refactor: undefined body support

--- app/modules/quiz/get/GetQuizController.ts
+++ app/modules/quiz/get/GetQuizController.ts
@@ -18,7 +18,7 @@ export default class GetQuizController extends BaseAPIGatewayController<QuizDto>
   protected async run(event: APIGatewayProxyEventV2): Promise<ApiGatewayControllerResponse<QuizDto>> {
     const type: number = event.pathParameters?.type as unknown as number ?? 1;
     const quiz: Quiz = await this.getQuizRepository.execute(type);
-    const response = new ApiGatewayControllerResponse<QuizDto>(quiz.toJson());
+    const response = new ApiGatewayControllerResponse<QuizDto>({body: quiz.toJson()});
     return response;
   }
 }
