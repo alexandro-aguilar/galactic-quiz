b2334b7a9cd7eb4d043efbdab84dbb77d32236df
Alexandro Aguilar <alexandro.aguilar@caylent.com>
2025-03-26 13:32:12 -0600

    refactor: add inversion of control

--- app/modules/user/register/RegisterUserHandler.ts
+++ app/modules/user/register/RegisterUserHandler.ts
@@ -1,8 +1,11 @@
-import UserRegisterController from './RegisterUserController';
+import 'reflect-metadata';
+import container from './container';
 import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import types from './types';
+import BaseAPIGatewayController from '@app/core/infrastructure/controller/BaseAPIGatewayController';
 
 export const handler = async (event: APIGatewayProxyEventV2) => {
-  const userRegisterController = new UserRegisterController();
+  const userRegisterController: BaseAPIGatewayController<void> = container.get(types.RegisterUserController);
   const response = await userRegisterController.execute(event);
   return response;
 };
