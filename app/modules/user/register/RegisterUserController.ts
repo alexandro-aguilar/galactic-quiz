88e185047b720deb4dd4ec5a39c547e1d1ed0f50
Alexandro Aguilar <alexandro.aguilar@caylent.com>
2025-03-26 13:32:59 -0600

    refactor: extend BaseAPIGatewayController

--- app/modules/user/register/RegisterUserController.ts
+++ app/modules/user/register/RegisterUserController.ts
@@ -1,21 +1,24 @@
-import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
 import User from './User';
 import UserRegisterRepository from './RegisterUserRepository';
 import HttpStatusCode from '../../../core/infrastructure/enum/httpStatusCode';
 import UserDto from './UserDto';
+import BaseAPIGatewayController from '@app/core/infrastructure/controller/BaseAPIGatewayController';
+import ApiGatewayControllerResponse from '@app/core/infrastructure/controller/ApiGatewayControllerResponse';
 
-export default class RegisterUserController {
+export default class RegisterUserController extends BaseAPIGatewayController<void> {
   constructor(
     private readonly userRegisterRepository = new UserRegisterRepository()
-  ) {}
+  ) {
+    super();
+  }
 
-  async execute(request: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> {
-    const body: UserDto = JSON.parse(request.body as string);
+  async run(event: APIGatewayProxyEventV2): Promise<ApiGatewayControllerResponse<void>> {
+    const body: UserDto = JSON.parse(event.body as string);
     const user = new User(body.email, body.practice, body.name);
     const registrationResponse: boolean = await this.userRegisterRepository.execute(user);
-
-    return {
-      statusCode: registrationResponse ? HttpStatusCode.CREATED : HttpStatusCode.OK
-    };
+    const statusCode = registrationResponse ? HttpStatusCode.CREATED : HttpStatusCode.OK;
+    const response = new ApiGatewayControllerResponse<void>({statusCode});
+    return response;
   }
 }
