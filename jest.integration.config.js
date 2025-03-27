d7268ec56029736462b83c0937a26c22b98d3853
Alexandro Aguilar <alexandro.aguilar@caylent.com>
2025-03-27 16:32:35 -0600

    chore: update

---
+++ jest.integration.config.js
@@ -0,0 +1,17 @@
+export const preset = 'ts-jest';
+export const testEnvironment = 'node';
+export const moduleFileExtensions = ['ts', 'tsx', 'js', 'jsx'];
+export const testRegex = '\\integration.(test|spec)\\.[t]s$';
+export const roots = ['<rootDir>/test'];
+export const transform = {
+  '^.+\\.tsx?$': 'ts-jest'
+};
+export const collectCoverage = true;
+export const coverageDirectory = 'coverage';
+export const coverageReporters = ['lcov', 'text'];
+export const collectCoverageFrom = ['app/**/*.{ts,tsx}', '!**/node_modules/**', '!**/*.d.ts', '!**/I*'];
+export const moduleNameMapper = {
+  '^@app/(.*)$': '<rootDir>/app/$1', // Map @app to the project root
+  '^@utils/(.*)$': '<rootDir>/app/utils/$1', // Map @util to the project root
+};
+export const setupFiles = ['<rootDir>/jest.setup.js'];
