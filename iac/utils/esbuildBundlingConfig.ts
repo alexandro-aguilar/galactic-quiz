b1a14d6224ea952db8df00197ed7e1eb82e00f41
Alexandro Aguilar <alexandro.aguilar@caylent.com>
2025-03-29 10:50:51 -0600

    build: update external dependencies

--- iac/utils/esbuildBundlingConfig.ts
+++ iac/utils/esbuildBundlingConfig.ts
@@ -1,7 +1,9 @@
 const esbuildBundlingConfig = {
   externalModules: [
     'aws-sdk',
-    '@aws-sdk'
+    '@aws-sdk',
+    'inversify',
+    'reflect-metadata',
   ], // Exclude specific modules from bundling
   nodeModules: [],     // Include specific modules in the bundle
   target: 'node22',    // Set the target environment for esbuild
