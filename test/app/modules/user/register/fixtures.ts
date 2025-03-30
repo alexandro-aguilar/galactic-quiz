1df562ae0f12b13fe6597fd0fd0a524a25f5c501
Alexandro Aguilar <alexandro.aguilar@caylent.com>
2025-03-30 15:11:00 -0600

    test: update fixtures

--- test/app/modules/user/register/fixtures.ts
+++ test/app/modules/user/register/fixtures.ts
@@ -2,6 +2,6 @@ import HttpStatusCode from "@app/core/infrastructure/enum/httpStatusCode";
 
 export const fixtures = [
   { body: JSON.stringify({ "email": "example1@example.com", "name": "name", "practice": 1 }), headers: {}, expectedStatus: HttpStatusCode.CREATED },
-  // { body: JSON.stringify({ "email": "example1@example.com", "name": "name", "practice": 1 }), headers: {}, expectedStatus: HttpStatusCode.OK },
-  // { body: JSON.stringify({ "email": "example2@example.com", "name": "name", "practice": 2 }), headers: {}, expectedStatus: HttpStatusCode.CREATED },
+  { body: JSON.stringify({ "email": "example1@example.com", "name": "name", "practice": 1 }), headers: {}, expectedStatus: HttpStatusCode.OK },
+  { body: JSON.stringify({ "email": "example2@example.com", "name": "name", "practice": 2 }), headers: {}, expectedStatus: HttpStatusCode.CREATED },
 ];
