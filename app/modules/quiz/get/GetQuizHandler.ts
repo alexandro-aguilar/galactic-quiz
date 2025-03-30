e3bebcca30216f3bb008254525ab0a8c7c0ad1b0
Alexandro Aguilar <alexandro.aguilar@caylent.com>
2025-03-30 15:31:40 -0600

    refactor: update handler injection

--- app/modules/quiz/get/GetQuizHandler.ts
+++ app/modules/quiz/get/GetQuizHandler.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata';
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
-import GetQuizController from './GetQuizController';
 import container from './container';
+import BaseAPIGatewayController from '@app/core/infrastructure/controller/BaseAPIGatewayController';
+import QuizDto from './QuizDto';
+import types from './types';
 
 export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
-  const getQuizController = container.get<GetQuizController>(GetQuizController);
+  const getQuizController = container.get<BaseAPIGatewayController<QuizDto>>(types.GetQuizController);
   const response = await getQuizController.execute(event);
   return response;
 };
