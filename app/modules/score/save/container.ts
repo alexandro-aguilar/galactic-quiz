f2c65392f32895dc24a4602495773c1f4085e0bb
Roberto Pérez Guzmán <roberto.perez.guzman@hotmail.com>
2025-04-02 22:05:48 +0000

    fix: container + UseCase

--- app/modules/score/save/container.ts
+++ app/modules/score/save/container.ts
@@ -2,11 +2,13 @@ import { Container } from 'inversify';
 import types from './types';
 import SaveScoreController from './SaveScoreController';
 import SaveScoreRepository from './SaveScoreRepository';
+import SaveScoreUseCase from './SaveScoreUseCase';
 import ResponseMapper from '@app/core/infrastructure/controller/ResponseMapper';
 
 const container = new Container();
 
 container.bind(types.SaveScoreController).to(SaveScoreController);
+container.bind(types.SaveScoreUseCase).to(SaveScoreUseCase);
 container.bind(types.SaveScoreRepository).to(SaveScoreRepository);
 container.bind(types.ResponseMapper).to(ResponseMapper);
 
